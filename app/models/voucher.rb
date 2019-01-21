fcf3ab30c5c3a1c3e61d1728e8b433ca461089d8
Mike Holford <mike@holfy.io>
2019-01-21 17:41:17 +0200

    updated gifti url

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -5,7 +5,7 @@ class Voucher < ApplicationRecord
   after_create :save_screenshot
 
   def save_screenshot
-    voucher_url = Screenshot.new({url: "#{Rails.root}vouchers/#{self.id}/key=#{self.user.secret_key}",thumbnail_max_width: 700,viewport: "700x300"}).url
+    voucher_url = Screenshot.new({url: "https://gifti.club/vouchers/#{self.id}/key=#{self.user.secret_key}",thumbnail_max_width: 700,viewport: "700x300"}).url
     self.update(image: voucher_url)
   end
 
